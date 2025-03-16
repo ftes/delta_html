83f95ff5f109ef3e13848ee8c2560ce8b73617af
Fredrik Teschke <f@ftes.de>
2025-03-16 06:44:02 +0100

    mix new

---
+++ lib/delta_html.ex
@@ -0,0 +1,18 @@
+defmodule DeltaHtml do
+  @moduledoc """
+  Documentation for `DeltaHtml`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DeltaHtml.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/delta_html_test.exs
@@ -0,0 +1,8 @@
+defmodule DeltaHtmlTest do
+  use ExUnit.Case
+  doctest DeltaHtml
+
+  test "greets the world" do
+    assert DeltaHtml.hello() == :world
+  end
+end
