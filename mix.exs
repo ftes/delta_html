fe85e8a9ecdc196c7862dce2a9527ff728363c2c
Fredrik Teschke <f@ftes.de>
2025-03-18 07:06:51 +0100

    Release 0.4.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule DeltaHtml.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.4.0"
   @source_url "https://github.com/ftes/delta_html"
   @description """
   Convert Quill (Slab) Delta document format to HTML.
